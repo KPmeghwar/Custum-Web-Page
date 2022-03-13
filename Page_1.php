3cbc19cbc11742f3ecb443ac26ed79afd846c321
Kailash Meghwar <premanikailash97@gmail.com>
2022-03-13 19:09:11 +0500

    Custum-Web-Page

---
+++ Page_1.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>..:: Three Column Layout ::..</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="color.css">
+</head>
+<body>
+	<div class="row">
+		<div class="col-12 " style="border: 3px solid black">
+			<h1 align="center">Header</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-3 "style="border: 3px solid black">
+			<h2  style="border:2px solid green ;text-align: center; height: 250px; margin:30px">Menu Here</h2>
+
+			<p style="text-align:justify; border:2px solid blue;margin:30px ;height: 400px;" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+
+		<div class="col-6 ">
+			 <div class="col-12 " style="border: 2px solid black;height: 50px;">
+			 	<h2 style="text-align: center;">Some Content here</h2>
+
+
+
+
+			 </div>
+			<div class="col-3" style="border: 2px solid black; height: 200px;  width: 250px;margin: 30px;">
+				<h2 align="center">img1</h2>
+			</div>
+			<div class="col-3" style="border: 2px solid black; height: 200px;  width: 250px;margin:30px">
+				<h2 align="center">img2</h2>
+			</div>
+            <div class="col-12">
+
+            	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        </div>          
+			</div>
+
+	<div class="col-3 "style="border: 3px solid black">
+		   <h2 align="center" style="border: 2px solid orange; height: 250px ; margin:30px">Login-Form
+		   <table border="2px" align="center" width="70px">
+		   	<tr>
+			<td><input type="name" name="name" placeholder="usename">
+			</td>
+			<tr>
+			<td><input type="pass" name="name" placeholder="password"></td>
+		    </tr>
+			<td colspan="3" align="center"><input type="submit" name="name" value="Submit" ></td>
+		</table>
+		</h2>
+
+		<p style="text-align:justify; border:2px solid blue;margin:30px ;height: 400px;" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12 " style="border: 3px solid black">
+			<h1 align="center">Footer</h1>
+		</div>
+	</div>
+
+</body>
+</html>
